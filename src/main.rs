79664843b85bb03bd54dc17e0771bcdb6162efd2
Mike English <mike.english@gmail.com>
2023-08-14 18:13:44 -0400

    Blinking LED
    This one actually blinks!
    
    (example code from stm32l1xx-hal)

--- src/main.rs
+++ src/main.rs
@@ -5,31 +5,28 @@
 
 extern crate cortex_m;
 extern crate cortex_m_rt as rt;
+extern crate panic_semihosting;
 extern crate stm32l1xx_hal as hal;
 
-use core::panic::PanicInfo;
-use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::digital::v2::ToggleableOutputPin;
 use hal::prelude::*;
+use hal::rcc::Config;
 use hal::stm32;
 use rt::entry;
 
 #[entry]
 fn main() -> ! {
     let dp = stm32::Peripherals::take().unwrap();
+    let cp = cortex_m::Peripherals::take().unwrap();
+
+    let rcc = dp.RCC.freeze(Config::default());
+    let mut delay = cp.SYST.delay(rcc.clocks);
+
     let gpiob = dp.GPIOB.split();
-    let mut led = gpiob.pb7.into_push_pull_output();
+    let mut led = gpiob.pb6.into_push_pull_output();
 
     loop {
-        for _ in 0..1_000 {
-            led.set_high().unwrap();
-        }
-        for _ in 0..1_000 {
-            led.set_low().unwrap();
-        }
+        led.toggle().unwrap();
+        delay.delay(300.ms());
     }
-}
-
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
-    loop {}
 }
